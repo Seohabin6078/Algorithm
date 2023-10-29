d4e44caf38e9db8caab6dc3b28fcca9574150ba5
Seohabin6078 <117034592+Seohabin6078@users.noreply.github.com>
2023-10-29 20:27:41 +0900

    [Silver I] Title: 곱셈, Time: 120 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.java
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -6,27 +6,24 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         long a = Long.parseLong(st.nextToken());
         long b = Long.parseLong(st.nextToken());
         long c = Long.parseLong(st.nextToken());
-        long mod = mod(a, b, c);
-        System.out.println(mod);
+        System.out.println(func(a, b, c));
     }
 
-    public static long mod(long a, long b, long c) {
+    private static long func(long a, long b, long c) { // a를 b번 곱하고 c로 나눈 나머지를 구하는 함수
         if (b == 1) {
             return a % c;
         }
 
-        long temp = mod(a, b / 2, c);
+        long val = func(a, b / 2, c);
 
-        if (b % 2 == 1) {
-            return ((temp * temp % c) * (a % c)) % c;
+        if (b % 2 == 0) {
+            return val * val % c;
         }
 
-        return temp * temp % c;
+        return (val * val % c) * a % c;
     }
 }
