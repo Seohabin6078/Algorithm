8fd0d45ac0302b0890b1c7b264a97e650b23dde9
Seohabin6078 <117034592+Seohabin6078@users.noreply.github.com>
2023-11-01 15:05:22 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 304 ms, Memory: 27176 KB -BaekjoonHub

---
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static int[][][] memo = new int[21][21][21];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            if (a == -1 && b == -1 && c == -1) {
+                break;
+            }
+            int answer = w(a, b, c);
+            sb.append("w(").append(a).append(", ").append(b).append(", ").append(c).append(") = ").append(answer);
+            sb.append("\n");
+        }
+        System.out.print(sb);
+    }
+
+    private static int w(int a, int b, int c) {
+        if (a > 0 && b > 0 && c > 0 && a <= 20 && b <= 20 && c <= 20 && memo[a][b][c] != 0) {
+            return memo[a][b][c];
+        }
+
+        if (a <= 0 || b <= 0 || c <= 0) {
+            return 1;
+        }
+
+        if (a > 20 || b > 20 || c > 20) {
+            return memo[20][20][20] = w(20, 20, 20);
+        }
+
+        if (a < b && b < c) {
+            return memo[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
+        }
+
+        return memo[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
+    }
+}
